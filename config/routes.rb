33b5b80c1466b758f8f585d77f3cd3712b469477
hao truong <haotv03@gmail.com>
2019-09-17 23:10:51 +0700

    Webapp: Mutilanguage with I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get "/about" => "abouts#show"
-  root "home_pages#index"
-  namespace :admin do
-    mount Ckeditor::Engine => "/ckeditor"
-    devise_for :admins, path: "", path_names: { sign_in: "login", sign_out: "logout" }
-    root "dashboards#index"
-    resources :posts
-    resources :abouts
+  scope "(:locale)", locale: /en|vi/ do
+    get "/about" => "abouts#show"
+    root "home_pages#index"
+    namespace :admin do
+      mount Ckeditor::Engine => "/ckeditor"
+      devise_for :admins, path: "", path_names: { sign_in: "login", sign_out: "logout" }
+      root "dashboards#index"
+      resources :posts
+      resources :abouts
+    end
   end
 end
