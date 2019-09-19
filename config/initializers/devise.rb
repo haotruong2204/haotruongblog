0c759bc52412b668f80339921c2cf6df214b8262
hao truong <haotv03@gmail.com>
2019-09-19 17:26:05 +0700

    Webapp: Fix login fb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -298,5 +298,5 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
                   scope: "public_profile,email",
-                  callback_url: "https://#{ENV['SITE_DOMAIN']}/accounts/auth/facebook/callback"
+                  callback_url: "#{ENV['SITE_DOMAIN']}/accounts/auth/facebook/callback"
 end
