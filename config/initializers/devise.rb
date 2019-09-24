da9fa333aae970c74066369d124dd5088312de56
hao truong <haotv03@gmail.com>
2019-09-24 16:42:47 +0700

    Webapp: Fix login devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,5 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
-                  scope: "public_profile,email"
+                  scope: "public_profile,email",
+                  callback_url: "https://#{ENV['SITE_DOMAIN']}/users/auth/facebook/callback"
 end
